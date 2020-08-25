6de2fc6168b99c2d75535006697f687b3b34118a
irisyou55 <627623528@qq.com>
2020-08-25 22:07:25 +0800

    创建configmap、创建一定数量的pod、将configmap的数据添加到pod里数据卷中的特定路径中

--- src/main/java/ecnu/db/k8s/CreatePod.java
+++ src/main/java/ecnu/db/k8s/CreatePod.java
@@ -1,6 +1,5 @@
 package ecnu.db.k8s;
 
-import io.kubernetes.client.Copy;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
@@ -13,14 +12,11 @@ import io.kubernetes.client.util.Yaml;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 /**
  * @author youshuhong
- * 创建configmap、创建一定数量的pod，并将指定文件上传到pod的指定目录中
+ * 创建configmap、创建一定数量的pod、将configmap的数据添加到pod里数据卷中的特定路径中
  */
 public class CreatePod {
     public static void main(String[] args)
@@ -29,76 +25,22 @@ public class CreatePod {
         String configPath = "config";
         ApiClient client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(new FileReader(configPath))).build();
         Configuration.setDefaultApiClient(client);
-        CoreV1Api api = new CoreV1Api();
-        V1PodList list = new V1PodList();
-        //pod数量
+        CoreV1Api coreV1Api = new CoreV1Api();
+        //设定要创建的pod的数量
         int podNum = 1;
         //创建configmap
-        File file = new File("yaml/kustomization.yaml");
+        File file = new File("src/main/resources/kustomization.yaml");
         V1ConfigMap yamlConfigMap = (V1ConfigMap) Yaml.load(file);
-        V1ConfigMap createConfigMap = api.createNamespacedConfigMap("default", yamlConfigMap, null, null, null);
-
-        //创建pod里env变量
-        V1EnvFromSource envFromSource = new V1EnvFromSource();
-        V1ConfigMapEnvSource configMapEnvSource = new V1ConfigMapEnvSource();
-        configMapEnvSource.setName(Objects.requireNonNull(createConfigMap.getMetadata()).getName());
-        envFromSource.configMapRef(configMapEnvSource);
-
-        String path = System.getProperty("java.class.path");//jar文件本地相对路径
-
+        coreV1Api.createNamespacedConfigMap("default", yamlConfigMap, null, null, null);
         //创建一定数量的pod
         for (int i = 0; i < podNum; i++) {
-            Map<String, String> podMap = new HashMap<String, String>();
-            podMap.put("app", "pod-" + i);
-
-            V1Pod pod = new V1PodBuilder()
-                    .withNewMetadata()
-                    .withName("pod-" + i)
-                    .withLabels(podMap)
-                    .endMetadata()
-                    .withNewSpec()
-                    .addNewContainer()
-                    .withName("container-1")
-                    .withImage("openjdk")
-                    .withImagePullPolicy("IfNotPresent")
-                    .withEnvFrom(envFromSource)
-                    .addNewVolumeMount()
-                    .withName("shared")
-                    .withMountPath("/tmp/files")
-                    .endVolumeMount()
-                    .withCommand("/bin/bash", "-c", "--")
-                    .withArgs("while true; do sleep 30; done;")
-                    .endContainer()
-                    .addNewContainer()
-                    .withName("container-2")
-                    .withImage("openjdk")
-                    .withImagePullPolicy("IfNotPresent")
-                    .addNewVolumeMount()
-                    .withName("shared")
-                    .withMountPath("/var/files")
-                    .endVolumeMount()
-                    .withCommand("/bin/bash", "-c", "--")
-                    .withArgs("java -jar " + path + ";while true; do sleep 30; done")
-                    .endContainer()
-                    .addNewVolume()
-                    .withName("shared")
-                    .endVolume()
-                    .endSpec()
-                    .build();
-            api.createNamespacedPod("default", pod, null, null, null);
-            list.addItemsItem(pod);
+            File filePod = new File("src/main/resources/create-pod.yaml");
+            V1Pod yamlPod = (V1Pod) Yaml.load(filePod);
+            V1ObjectMeta objectMeta = yamlPod.getMetadata();
+            objectMeta.setName(Objects.requireNonNull(yamlPod.getMetadata().getName())+ '-' + i+1);
+            yamlPod.setMetadata(objectMeta);
+            coreV1Api.createNamespacedPod("default", yamlPod, null, null ,null);
         }
-
-        //等待pod的status更新为Running
-        Thread.sleep(3000);
-        Copy copy = new Copy();
-        //将jar文件传入pod
-        for (V1Pod item : list.getItems()) {
-            copy.copyFileToPod("default", Objects.requireNonNull(item.getMetadata()).getName(),
-                    Objects.requireNonNull(item.getSpec()).getContainers().get(0).getName(),
-                    Paths.get("/Users/irisyou/Documents/k8s-pod/" + path), Paths.get("/" + path));
-        }
-        //断开链接
         client.getHttpClient().connectionPool().evictAll();
 
     }
