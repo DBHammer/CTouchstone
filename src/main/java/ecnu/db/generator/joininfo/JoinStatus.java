dba090c88f2db41c81d5024ca03afbd52b5323fe
ylht <wqs.97@icloud.com>
2021-12-22 15:23:46 +0800

    [feature]: add join status

---
+++ src/main/java/ecnu/db/generator/joininfo/JoinStatus.java
@@ -0,0 +1,30 @@
+package ecnu.db.generator.joininfo;
+
+import java.util.Arrays;
+
+public record JoinStatus(boolean[] status) implements Comparable<JoinStatus> {
+    @Override
+    public String toString() {
+        return "JoinStatus{" +
+                "status=" + Arrays.toString(status) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        JoinStatus that = (JoinStatus) o;
+        return Arrays.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(status);
+    }
+
+    @Override
+    public int compareTo(JoinStatus o) {
+        return this.hashCode() - o.hashCode();
+    }
+}
