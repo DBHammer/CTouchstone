f27eff61049f85d5547e7829d840987232970526
ylht <wqs.97@icloud.com>
2021-12-30 09:09:42 +0800

    [enhance]: move data write to DataWriter

---
+++ src/main/java/ecnu/db/generator/DataWriter.java
@@ -0,0 +1,44 @@
+package ecnu.db.generator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class DataWriter {
+
+    String outputPath;
+    int generatorId;
+
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    public void addWriteTask(String schemaName, List<StringBuilder> rowData) {
+        executorService.submit(() -> {
+            try {
+                Files.write(Paths.get(outputPath + "/" + schemaName + generatorId), rowData,
+                        StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void reset(){
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    public DataWriter(String outputPath, int generatorId) {
+        this.outputPath = outputPath;
+        this.generatorId = generatorId;
+    }
+
+    public boolean waitWriteFinish() throws InterruptedException {
+        executorService.shutdown();
+        return executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+    }
+
+}
