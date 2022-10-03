549a28e6de5c6f0afc395e0522bf327af42381d2
smartgood1 <820842931@qq.com>
2022-10-03 13:34:31 +0800

    [enhance] replace stream compute in MathNode to for loop

--- src/main/java/ecnu/db/generator/constraintchain/filter/arithmetic/MathNode.java
+++ src/main/java/ecnu/db/generator/constraintchain/filter/arithmetic/MathNode.java
@@ -3,7 +3,6 @@ package ecnu.db.generator.constraintchain.filter.arithmetic;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class MathNode extends ArithmeticNode {
 
@@ -19,12 +18,27 @@ public class MathNode extends ArithmeticNode {
     public double[] calculate() {
         double[] leftValue = leftNode.calculate();
         double[] rightValue = rightNode.calculate();
-        IntStream indexStream = IntStream.range(0, leftValue.length);
         switch (type) {
-            case MUL -> indexStream.forEach(i -> leftValue[i] *= rightValue[i]);
-            case DIV -> indexStream.forEach(i -> leftValue[i] /= rightValue[i] == 0 ? Double.MIN_NORMAL : rightValue[i]);
-            case PLUS -> indexStream.forEach(i -> leftValue[i] += rightValue[i]);
-            case MINUS -> indexStream.forEach(i -> leftValue[i] -= rightValue[i]);
+            case MUL -> {
+                for (int i = 0; i < leftValue.length; i++) {
+                    leftValue[i] *= rightValue[i];
+                }
+            }
+            case DIV -> {
+                for (int i = 0; i < leftValue.length; i++) {
+                    leftValue[i] /= rightValue[i] == 0 ? Double.MIN_NORMAL : rightValue[i];
+                }
+            }
+            case PLUS -> {
+                for (int i = 0; i < leftValue.length; i++) {
+                    leftValue[i] += rightValue[i];
+                }
+            }
+            case MINUS -> {
+                for (int i = 0; i < leftValue.length; i++) {
+                    leftValue[i] -= rightValue[i];
+                }
+            }
             default -> throw new UnsupportedOperationException();
         }
         return leftValue;
