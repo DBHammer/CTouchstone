f728edfd27b85ee8f1fa886b83aae9a4975c8d00
ylht <wqs.97@icloud.com>
2023-02-23 10:18:54 +0800

    [refactor] move jdk to jdk 19

--- src/main/java/ecnu/db/LanguageManager.java
+++ src/main/java/ecnu/db/LanguageManager.java
@@ -5,8 +5,7 @@ import java.util.ResourceBundle;
 
 public class LanguageManager {
     private static final LanguageManager INSTANCE = new LanguageManager();
-    //private final Locale lc = Locale.getDefault();
-    private final Locale lc = new Locale("zh", "CN");
+    private final Locale lc = Locale.of("zh", "CN");
     private final ResourceBundle rb = ResourceBundle.getBundle("messageResource", lc);
 
     public static LanguageManager getInstance() {
