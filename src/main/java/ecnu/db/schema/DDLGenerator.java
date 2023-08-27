20e01bcc1becb5f0348a86283a7df8972694d00d
smartgood1 <820842931@qq.com>
2023-08-27 21:38:03 +0800

    [feacher] DDLGenerator

--- src/main/java/ecnu/db/schema/DDLGenerator.java
+++ src/main/java/ecnu/db/schema/DDLGenerator.java
@@ -59,7 +59,7 @@ public class DDLGenerator implements Callable<Integer> {
         StringBuilder importData = new StringBuilder("\\c " + dataBase + ";\n");
         for (Map.Entry<String, Table> tableName2Schema : TableManager.getInstance().getSchemas().entrySet()) {
             String tableName = tableName2Schema.getKey();
-            String inData = "\\Copy " + tableName.split("\\.")[1] + " FROM PROGRAM" + "'" + "cat ./data/" + tableName + "-*" + "' DELIMITER ',';\n";
+            String inData = "\\Copy " + tableName.split("\\.")[1] + " FROM PROGRAM" + "'" + "cat ./data/" + tableName.split("\\.")[1] + "_0.txt" + "' DELIMITER ',' " + "NULL 'null';\n";
             importData.append(inData);
         }
         CommonUtils.writeFile(this.importData, importData.toString());
